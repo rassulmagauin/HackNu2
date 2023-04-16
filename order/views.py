7bd8dad0e47c200c05949bb2b3aa84202764944e
Rassul Magauin <rassulmagauin@gmail.com>
2023-04-16 06:42:03 +0600

    Cost of Delivery

--- order/views.py
+++ order/views.py
@@ -8,34 +8,34 @@ import os
 import requests
 import math
 from django.http import HttpResponse
+import json
 
 def search(*params):
-    address = ''
-    for param in params:
-        if param:
-            address += param + ' '
+    address = ' '.join(str(param) for param in params)
     
     url = "https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi"
     headers =  {
 	    "X-RapidAPI-Key": "4b6fc0a3camsh3650402cb52ad17p18c16ajsn00f3e37ee952",
 	    "X-RapidAPI-Host": "address-from-to-latitude-longitude.p.rapidapi.com"
     }
-    response = requests.request("GET", url, headers=headers, params={"address": address})
-    return response["Results"][0].latitude, response["Results"][0].longitude
+    response = requests.get(url, headers=headers, params={"address": address})
+    # return response(["Results"][0], response["Results"][0])
     # return response
+    data = json.loads(response.content)
+    return [data["Results"][0]["latitude"], data["Results"][0]["longitude"]]
 
 def def_cost():
-    initial_address = os.environ.get("ADDRESS")
+    initial_address = 'Kazakhtan Astana Kerey Janibek 4/1'
     print(initial_address)
     return search(initial_address)
 
 def get_cost(*params):
     initial_address = os.environ.get("ADDRESS")
-    lat1, lng1 = search(*params)
+    lat1, lng1 = search(params)
     lat2, lng2 = search(initial_address)
     lat, lng = lat2 - lat1, lng2 - lng1
 
-    return 'cost is: ' + math.sqrt(lat*lat + lng*lng)
+    return 'cost is: ' + str(int(math.sqrt(lat*lat + lng*lng) * 15 + 250))
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all().order_by('date')
@@ -45,7 +45,6 @@ class OrderViewSet(viewsets.ModelViewSet):
         serializer = OrderSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            # cost = get_cost(serializer.validated_data['region'], serializer.validated_data['city'])
-            cost = def_cost()
-            return HttpResponse({cost}, status.HTTP_201_CREATED)
+            cost = get_cost(serializer.validated_data['region'], serializer.validated_data['city'])
+            return HttpResponse(cost, status.HTTP_201_CREATED)
         return HttpResponse(serializer.errors, status.HTTP_400_BAD_REQUEST)
